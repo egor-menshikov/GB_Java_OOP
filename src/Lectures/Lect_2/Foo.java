4a72e25b0b2193aa2c947db70446f8824a6eed8f
Egor Menshikov <r3born.fs@gmail.com>
2023-07-18 01:47:42 +0300

    lect_2 done

---
+++ src/Lectures/Lect_2/Foo.java
@@ -0,0 +1,10 @@
+package Lectures.Lect_2;
+
+public class Foo {
+    private int value;
+    public static int count;
+
+    static {
+        count = 0;
+    }
+}
---
+++ src/Lectures/Lect_2/Main.java
@@ -0,0 +1,7 @@
+package Lectures.Lect_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
